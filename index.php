8cd8526adf0915db71c0b65ae08dceee7d011454
Xedris <52141557+Xedris@users.noreply.github.com>
2019-07-15 08:32:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ p    {color: Cyan;}
 <h1>FORTUNO, ADRIAN CHRISTIAN O</h1>
 <P>Hi there, names Adrian and this is a short Bio about me and what I am. I just recently turned 20 last month on the 18th of june and it has been a weird 20 years of life. I've never much fit in with regular people, I've always weirded them out with the stuff I like since it's not something that's normal for them.</P>
 <a href="LABWORK1_Fortuno.php">My Labwork 1</a>
-<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
-<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
+<a href="new-1.php">My Labwork 1</a>
+<a href="new-2.php">My Labwork 1</a>
 
 
 </body>
