f3491929922a12c31e35e86c5734266cf769a5af
Xedris <52141557+Xedris@users.noreply.github.com>
2019-08-08 21:02:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <a href="new-2.php">My Likes and dislikes </a>
 <a href="Activity2.php">My Favorite apps</a>
 <a href="Reflections.php">Reflections</a>  
+<a href="Activity3.php">1000 Floor Elevator</a>
+
 <style>
 
 body {background-color: darkgreen;}
