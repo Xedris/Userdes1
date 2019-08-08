c78192e17dcfc3ce35a2134d2f228a0e531263a1
Xedris <52141557+Xedris@users.noreply.github.com>
2019-08-08 21:00:12 +0800

    Create Activity3.php

---
+++ Activity3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><b> My Favorite Applications </b> </title>
+<a href="index.php">HOMEPAGE</a>
+<a href="new-2.php">My Likes and dislikes</a>
+<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
+<a href="Reflections.php">Reflections</a>
+
+</head>
+<body style="background-color:Black;">
+<h1 style="color:RosyBrown;"> 1st application</h1>
+<p1 style="color:RosyBrown;">The first thing I would do to create an Elevator for a building with 1000 floors would be to think of how 
+the elevator would be able to traverse each floor. The solution would be having 3 main gears in the system, each gear would have to 
+correspond with a number placement. 1 gear would be for the ones position, the 2nd gear for the tens position and the last gear would 
+be for the hundreds position and the 1000th floor..</p1>
+<p2 style="color:RosyBrown;">For how people would choose which floor to go to, instead of the conventional buttons for each floor because 
+it would be a hassle to have 1000 buttons for each floor, it would be more convenient to install a digital numpad for use. The digital 
+numpad would be easier to use in the elevator allowing people to just input the floor number on the touchpad and if they decide not to
+go to that floor and want to change it, there would be a second screen right next to numpad that would show a list of the floors that 
+have already been inputted and if they want to cancel that floor all they have to do is select it and there would be a pop-up that 
+would appear asking if they want to cancel that floor. As for people who want to get on to an elevator and they want to know which
+floor the elevator is on there would be another screen on the outside below the elevator call buttons, which would show what direction
+the elevator is going, what floor it is currently on and which floors have been inputted and what the next floor is.</p2>
+<p3 style="color:RosyBrown;"> Since the elevator is for a building with 1000 floors it’s obvious that one elevator alone is not enough
+to accommodate the entire building, I would have 5 different sets of elevators situated in the North, South, East and West side of the 
+building and the last set of elevators would be in the very center of the building. Each set is equivalent to 7 elevators so in all there
+would be 35 elevators in the entire Building. As for the elevator itself, instead of the usual elevator used for malls and condominiums,
+it would instead be a dover hyrdraulic freight elevator, the reason for this would be because there would be possibly over a thousand
+people in the building it would be much more convenient to use a freight elevator because it would be able to handle the larger amount
+of people and the added weight to it. As for the design of the elevator's interior because it is a freight elevator it would allow for 
+the addition of benches along the rails and creating a sort of aisle system inside the elevator, for PWD there would be a special
+designated area. There would also be a first aid kit put into each corner of the elevator for emergencies. To help people pass the 
+time there would be music playing inside the elevator for each person to use, and most importantly there would be a wifi router installed 
+in each elevator to allowing each person to connect and pass the time more efficiently especially for the higher most floors where cell
+signal would start to disappear</p3>
+</body>
+</html>
