a061de21dfa698cb37f0bfdd47620b055cc02eac
Xedris <52141557+Xedris@users.noreply.github.com>
2019-08-08 17:31:26 +0800

    Create Reflections.php

---
+++ Reflections.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><b> Reflections </b> </title>
+<a href="index.php">HOMEPAGE</a>
+<a href="new-2.php">My Likes and dislikes</a>
+<a href="LABWORK1_FORTUNO.php">Labwork 1</a>
+
+</head>
+<body style="background-color:Black;">
+<h1 style="color:Lime;"> Reflection 1</h1>
+<p1 style="color:Lime;"> The first week of the class was actually quite surprising since I didn’t expect to be using trello, 
+I didn’t even know trello was a thing at first until Ms showed it to us. As for the activities I liked doing them since it allows us 
+to express ourselves. I liked doing the one that asked us to talk about or favorite app since I like to talk about the features each
+one gives and uses. For the teacher I like the way Ms is doing the class but only doing activities is a bit weird but I think I can 
+adjust well.</p1>
+
+<h2 style="color:DeepPink;"> Reflection 2 </h2>
+<p2 style="color:DeepPink;">My reflection about the 2nd week of the class is that things are moving too fast and that there is 
+almost no time to understand everything. There is also to many things to use at the same time especially Heroku since it’s hard to 
+use and understand. But as for the activities itself they are fun to do especially with how we must solve the problems. As for the 
+class itself it’s still nice to code but all the continous work is starting to bog us down.</p2>
+
+<h3 style="color:LightGreen;"> Reflection 3</h3>
+<p3 style="color:LightGreen;">So the moment ms gave us the Lab 1 activity I was excited to do it because it was something I haven’t done 
+in a long time and it was something I used to do before when I was in 3rd year highschool. When we all learned that we had to upload 
+the HTML into both heroku and github it was actually confusing on how to make heroku work especially since for so long we’ve been using
+only Github for our work and heroku was brand new for us to work with. I actually was starting to think that heroku was busted or 
+something since it had no user manual to use but once we got it to work it was actually easy to understand more or less</p3>
+
+<h4 style="color:MediumOrchid;"> Reflection 4</h4>
+<p4 style="color:MediumOrchid;">After the lesson when talking about personas and UI I really did not care much about what went on in the UI
+since I only cared about if it was useful or not. Even now I only care if the UI is simple enough to use and navigate for me.</p4>
+
+
+
+</body>
+</html>
