1e1efd25c794e9ef8bfb47339a26f7eac6732a97
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-09 07:34:24 +0800

    Create Activity-7.php

---
+++ Activity-7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><b> My Favorite Applications </b> </title>
+<a href="index.php">HOMEPAGE</a>
+<a href="new-2.php">My Likes and dislikes</a>
+<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
+<a href="Reflections.php">Reflections</a>
+<a href="Activity3.php">1000 Floor Elevator</a>
+
+</head>
+<img src="Activity.7.png" alt="You Are Hereby">
+
+
+</body>
+</html>
