0a3331dcb9d8d7ca4d88bbf133432a040ec044dd
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-09 07:59:06 +0800

    Create Activity-6.php

---
+++ Activity-6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><b> Personas </b> </title>
+<img src="Satanael.jpg" alt="Girl in a jacket" width="500" height="600">
+<a href="index.php">HOMEPAGE</a>
+<a href="new-2.php">My Likes and dislikes</a>
+<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
+<a href="Reflections.php">Reflections</a>
+<a href="Activity3.php">1000 Floor Elevator</a>
+<a href="Activity-7.php">Invitation</a>
+
+</head>
+<body style="background-color:Black;">
+<h1 style="color:Lime;"> 1st application</h1>
+<p1 style="color:Lime;"> I have two favorite applications, the first would have to be game master 5 because it’s the main
+tool I use when I play with my friends. I like using this app because it has all the tools I need to create
+and control our DnD games. It has all the items and rules already installed in it and it allows us to keep
+track of our own characters as well.</p1>
