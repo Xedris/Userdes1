d6d801597e9146ebdbf836b2c1b96eff2e94ec16
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-09 08:22:38 +0800

    Update Activity-6.php

--- Activity-6.php
+++ Activity-6.php
@@ -10,8 +10,8 @@
 <a href="Activity-7.php">Invitation</a>
 
 </head>
-<body style="background-color:Black;">
-<h1><img src="Satanael.jpg" alt="Girl in a jacket" width="300" height="400"></h1>
+<body background="Satanael.jpg">
+<h1><img src="Satanael.jpg" alt="Girl in a jacket" width="330" height="400"></h1>
 <h2 style="color:Lime;"> Persona 1</h2>
 <p1 style="color:Lime;"> Dwayne is a student here in Asia Pacific College. He spends lots of his time working on his projects in 
   his engineering class, and for the past few days, he’s been feeling rather loaded with work and exams. Now that exams are over, 
@@ -22,5 +22,28 @@ Dwayne asks one of his professors, Sir Isaac, about the lab’s hours, to which 
   set schedules and activities. He finds a slot that fits his schedule, and arrives with his friends at the appointed time to see some 
   others in the room. They find their reserved place and begin gaming.
 </p1>
+<h3 style="color:Lime;"> Persona 2</h3>
+  <p2> Sir Isaac is a Game Development Professor here at Asia Pacific College who also happens to be one of the founding members of 
+    Gaming Genesis. Aside from spending time gaming with his students, and moderating the activities in the room, he also uses the 
+    area for some of his classes when needed.
+On one particular day, he decides it would be best to deliver his lecture in the lab, but wants to do so without interfering with the 
+    time of the students. Seeing as how the reservation system prioritizes class lectures, he finds a free time slot that fits his class 
+    schedule and sets it. 
+One of his irregular students, Dwayne, sees the lecture-based reservation while browsing the website for a schedule, and recognizes that 
+    the room will be reserved entirely for that period. He accepts the case and is thankful that he was informed well ahead of time to 
+    choose another schedule.
+  </p2>
+<h4 style="color:Lime;"> Persona 2</h4>
+ <p3> Miss Mara is an employee here at Asia Pacific College as part of the ITRO. Aside from managing the websites and other systems 
+   that keep the school running, she is also partly responsible for watching over the borrowed equipment and peripherals of the school.
+Mara happens to also be one of the early developers of the Game laboratory Reservation System, and regularly uses it to monitor 
+   equipment use.
+On one of her checks over the room, she discovers that one of the Xbox controllers has been lightly damaged, and is in need of repairs.
+   She then checks the system to see the latest reservation involving the Xbox, and identifies it as the group scheduled by Dwayne. 
+She informs Isaac, who is one of the head moderators of the room. Isaac, who is a professor, recognizes Dwayne and approaches him
+   regarding the matter. He then finds out that Dwayne had mistakenly dropped the controller when heading out, but didn’t think the 
+   damage too severe. Seeing the case, Dwayne makes the appropriate payments for repairs.
+
+  </p3>  
 </body>
 </html>
