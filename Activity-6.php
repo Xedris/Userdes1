6736f3a95a0410fa0b4f019c03140ea1d51bdb48
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-09 08:08:06 +0800

    Update Activity-6.php

--- Activity-6.php
+++ Activity-6.php
@@ -11,11 +11,16 @@
 
 </head>
 <body style="background-color:Black;">
-  <h1><img src="Satanael.jpg" alt="Girl in a jacket" width="200" height="300"></h1>
-<h2 style="color:Lime;"> 1st application</h2>
-<p1 style="color:Lime;"> I have two favorite applications, the first would have to be game master 5 because it’s the main
-tool I use when I play with my friends. I like using this app because it has all the tools I need to create
-and control our DnD games. It has all the items and rules already installed in it and it allows us to keep
-track of our own characters as well.</p1>
-  </body>
+<h1><img src="Satanael.jpg" alt="Girl in a jacket" width="300" height="400"></h1>
+<h2 style="color:Lime;"> Persona 1</h2>
+<p1 style="color:Lime;"> Dwayne is a student here in Asia Pacific College. He spends lots of his time working on his projects in 
+  his engineering class, and for the past few days, he’s been feeling rather loaded with work and exams. Now that exams are over, 
+  he wants to spend some quality time with his friends after classes. 
+He then remembers his friend telling him about the Gaming Laboratory on the 5th floor, and that they should pay a visit one day. 
+  He checks his schedule and sees that he’s free to visit with his group later in the afternoon, but is unsure if the lab is available.
+Dwayne asks one of his professors, Sir Isaac, about the lab’s hours, to which he is told of the new web application that allows users to
+  set schedules and activities. He finds a slot that fits his schedule, and arrives with his friends at the appointed time to see some 
+  others in the room. They find their reserved place and begin gaming.
+</p1>
+</body>
 </html>
