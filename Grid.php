0c00f6e5fcbf9ebc4dfac2354bcc9d94a8829fe9
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-16 08:11:56 +0800

    Create Grid.php

---
+++ Grid.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><b>Grid Designs</b> </title>
+<a href="index.php">HOMEPAGE</a>
+<a href="new-2.php">My Likes and dislikes</a>
+<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
+<a href="Reflections.php">Reflections</a>
+<a href="Activity3.php">1000 Floor Elevator</a>
+<a href="Activity-6.php">Persona</a>
+<a href="Activity-7.php">Invitation</a>
+</head>
+<p1>3x3 Foundations</p1>
+<p2><img src="3x3 white and black.JPG" alt="3x3 foundations"></p2>
+<p3>The King of YOUTUBE</p3>
+<p4><img src="3x3 pictures.JPG" alt="The King Of YOUTUBE"></p4>
+<p5>4x4 Foundation</p5>
+<p6><img src="4x4 black and white.JPG" alt="4x4 Foundations"></p6>
+<p7>FATE GRAND ORDER</p7>
+<p8><img src="4x4 pictures.JPG" alt="FATE GRAND ORDER"></p8>
+</body>
+</html>
