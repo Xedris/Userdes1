85a035c69d4dbcdc50e41a16ae70511adaf25246
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-23 08:39:16 +0800

    Update UX-DESIGN-CERTIFICATES.php

--- UX-DESIGN-CERTIFICATES.php
+++ UX-DESIGN-CERTIFICATES.php
@@ -17,5 +17,11 @@
 <p4><img src="cert2.JPG" alt="INTERACTION DESIGN: PROJECTS AND PLATFORMS"></p4>
 <h1>CERTIFICATE 3</h1>
 <p6><img src="cert3.JPG" alt="UX FOUNDATIONS: INTERACTION DESIGN"></p6>
+<h1>Certificate 4</h1>
+<p2><img src="cert 4.JPG" alt="UX FOUNDATIONS: CONTENT STRATEGY"></p2>
+<h1>Certificate 5</h1>
+<p4><img src="cert 5.JPG" alt="INTERACTION DESIGN: PROJECTS AND PLATFORMS"></p4>
+<h1>CERTIFICATE 6</h1>
+<p6><img src="cert 6.JPG" alt="UX FOUNDATIONS: INTERACTION DESIGN"></p6>
 </body>
 </html>
