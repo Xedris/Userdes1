c3a157dd6dfeaecaae2e2c995ba06df0007989bc
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 21:24:35 +0800

    Update Activity3.php

--- Activity3.php
+++ Activity3.php
@@ -2,32 +2,38 @@
 <html>
 <head>
 <title><b> 1000 ELEVATORS!!!! </b> </title>
-<a href="index.php">HOMEPAGE</a>
-<a href="new-2.php">My Likes and dislikes</a>
-<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
-<a href="Reflections.php">Reflections</a>
-<a href="Activity-6.php">Persona</a>
-<a href="Activity-7.php">Invitation</a>
-<a href="Grid.php">Grid Designs</a>
-<a href="UX-DESIGN-CERTIFICATES.php">Vid Certificates</a>
+<a href="index.php"><font size="5">HOMEPAGE</font></a>
+<a href="new-2.php"><font size="5">My Likes and dislikes</font></a>
+<a href="LABWORK1_Fortuno.php"><font size="5">My Labwork 1</font></a>
+
+<a href="Activity-6.php"><font size="5">Persona</font></a>
+<a href="Activity-7.php"><font size="5">Invitation</font></a>
+<a href="Grid.php"><font size="5">Grid Designs</font></a>
+<a href="Reflections.php"><font size="5">Reflections</font></a>
+<a href="UX-DESIGN-CERTIFICATES.php"><font size="5">Vid Certificates</font></a>
 </head>
-<body style="background-color:Black;">
+body {
+  background-image: url('rwby.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
 <h1 style="color:RosyBrown;"> THE ELEVATOR</h1>
-<p1 style="color:CornflowerBlue;">The first thing I would do to create an Elevator for a building with 1000 floors would be to think of how 
+<p1 style="color:CornflowerBlue;"><font size="5">The first thing I would do to create an Elevator for a building with 1000 floors would be to think of how 
 the elevator would be able to traverse each floor. The solution would be having 3 main gears in the system, each gear would have to 
 correspond with a number placement. 1 gear would be for the ones position, the 2nd gear for the tens position and the last gear would 
-be for the hundreds position and the 1000th floor..</p1>
-  <h2></h2>
-<p2 style="color:CornflowerBlue;">For how people would choose which floor to go to, instead of the conventional buttons for each floor because 
+be for the hundreds position and the 1000th floor..</font></p1><br>
+<p2 style="color:CornflowerBlue;"><font size="5">For how people would choose which floor to go to, instead of the conventional buttons for each floor because 
 it would be a hassle to have 1000 buttons for each floor, it would be more convenient to install a digital numpad for use. The digital 
 numpad would be easier to use in the elevator allowing people to just input the floor number on the touchpad and if they decide not to
 go to that floor and want to change it, there would be a second screen right next to numpad that would show a list of the floors that 
 have already been inputted and if they want to cancel that floor all they have to do is select it and there would be a pop-up that 
 would appear asking if they want to cancel that floor. As for people who want to get on to an elevator and they want to know which
 floor the elevator is on there would be another screen on the outside below the elevator call buttons, which would show what direction
-the elevator is going, what floor it is currently on and which floors have been inputted and what the next floor is.</p2>
+the elevator is going, what floor it is currently on and which floors have been inputted and what the next floor is.</font></p2><br>
   <h3></h3>
-<p3 style="color:CornflowerBlue;"> Since the elevator is for a building with 1000 floors it’s obvious that one elevator alone is not enough
+<p3 style="color:CornflowerBlue;"><font size="5"> Since the elevator is for a building with 1000 floors it’s obvious that one elevator alone is not enough
 to accommodate the entire building, I would have 5 different sets of elevators situated in the North, South, East and West side of the 
 building and the last set of elevators would be in the very center of the building. Each set is equivalent to 7 elevators so in all there
 would be 35 elevators in the entire Building. As for the elevator itself, instead of the usual elevator used for malls and condominiums,
@@ -38,6 +44,15 @@ the addition of benches along the rails and creating a sort of aisle system insi
 designated area. There would also be a first aid kit put into each corner of the elevator for emergencies. To help people pass the 
 time there would be music playing inside the elevator for each person to use, and most importantly there would be a wifi router installed 
 in each elevator to allowing each person to connect and pass the time more efficiently especially for the higher most floors where cell
-signal would start to disappear</p3>
+  signal would start to disappear</font></p3>
+<a href="index.php"><font size="5">HOMEPAGE</font></a>
+<a href="new-2.php"><font size="5">My Likes and dislikes</font></a>
+<a href="LABWORK1_Fortuno.php"><font size="5">My Labwork 1</font></a>
+
+<a href="Activity-6.php"><font size="5">Persona</font></a>
+<a href="Activity-7.php"><font size="5">Invitation</font></a>
+<a href="Grid.php"><font size="5">Grid Designs</font></a>
+<a href="Reflections.php"><font size="5">Reflections</font></a>
+<a href="UX-DESIGN-CERTIFICATES.php"><font size="5">Vid Certificates</font></a>
 </body>
 </html>
