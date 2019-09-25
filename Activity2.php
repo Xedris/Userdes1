619e59304ad8ebef59ade7feea600947882118df
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 21:08:14 +0800

    Update Activity2.php

--- Activity2.php
+++ Activity2.php
@@ -28,7 +28,7 @@ body {
 tool I use when I play with my friends. I like using this app because it has all the tools I need to create
 and control our DnD games. It has all the items and rules already installed in it and it allows us to keep
 track of our own characters as well.</font></p1><br>
-  <p7><img src="GM5.jpg" alt="You Are Hereby" style="width:450px;height:400px;"></p7>
+  <p7><img src="GM5.jpg" alt="You Are Hereby" style="width:470px;height:400px;"></p7>
 
 <h1 style="color:Chartreuse ;"> 2nd application</h1>
 <p2 style="color:Chartreuse ;"><font size="5">My 2nd favorite application is spotify because it has a large collection of music from many different
@@ -36,25 +36,25 @@ genres and artists. This is one of my favorite apps because during my free time 
 love to listen to music to pass the time. One of the main downsides though about spotify is that you
 must listen through advertisements not unless you buy the premium version but overall it’s a very good
 music application.</font></p2><br>
- <p7><img src="SPOT.jpg" alt="You Are Hereby" style="width:450px;height:400px;"></p7>
+ <p7><img src="SPOT.jpg" alt="You Are Hereby" style="width:470px;height:400px;"></p7>
 <h1 style="color:MediumAquaMarine ;"> 3rd application</h1>
 <p3 style="color:MediumAquaMarine ;"><font size="5">Dropbox would have to be my third favorite application because it’s a quick and easy file sharing
 application. It allows me and others to upload important files online that way even if we lose the original
 copy its saved online for future use. It also allows us to share those files with other people and they can
 add their own files to it so they can share it with you</font></p3><br>
- <p7><img src="DROP.jpg" alt="You Are Hereby" style="width:450px;height:400px;"></p7>
+ <p7><img src="DROP.jpg" alt="You Are Hereby" style="width:470px;height:400px;"></p7>
 <h1 style="color:SeaGreen ;"> 4th application</h1>
 <p4 style="color:SeaGreen ;"><font size="5">Facebook is my fourth favorite app because it’s an easy way for me to pass the time when I have
 nothing to do plus, I can see what my friends have shared and react to what they posted. It’s also a way
 for us to comment on the posts and it can them to comment back to us.</font></p4><br>
- <p7><img src="FACE.jpg" alt="You Are Hereby" style="width:450px;height:400px;"></p7>
+ <p7><img src="FACE.jpg" alt="You Are Hereby" style="width:470px;height:400px;"></p7>
 <h1 style="color:NavajoWhite ;"> 5th application</h1>
 <p5 style="color:NavajoWhite ;"> <font size="5">Fate Grand Order is my last favorite app because it’s a good way for me to pass the time and even
 though it seems simple the game is quite difficult when the challenge quests come around because you
 have to plan your team and how to use them to their fullest. I also love the art style they use in the
 game because they have all their own distinct way of looking. The story is also something that has
 interested me because it isn’t something normally seen.</font></p5><br>
- <p7><img src="FATE.jpg" alt="You Are Hereby" style="width:450px;height:400px;"></p7><br>
+ <p7><img src="FATE.jpg" alt="You Are Hereby" style="width:470px;height:400px;"></p7><br>
 <a href="index.php"><font size="5">HOMEPAGE</font></a>
 <a href="new-2.php"><font size="5">My Likes and dislikes</font></a>
 <a href="LABWORK1_Fortuno.php"><font size="5">My Labwork 1</font></a>
