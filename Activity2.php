a7eba55c883049ea0bbeb90504d74d91db7c1529
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 10:15:41 +0800

    Update Activity2.php

--- Activity2.php
+++ Activity2.php
@@ -5,46 +5,48 @@
 <a href="index.php">HOMEPAGE</a>
 <a href="new-2.php">My Likes and dislikes</a>
 <a href="LABWORK1_Fortuno.php">My Labwork 1</a>
-<a href="Reflections.php">Reflections</a>
+
 <a href="Activity3.php">1000 Floor Elevator</a>
 <a href="Activity-6.php">Persona</a>
 <a href="Activity-7.php">Invitation</a>
 <a href="Grid.php">Grid Designs</a>
+<a href="Reflections.php">Reflections</a>
 <a href="UX-DESIGN-CERTIFICATES.php">Vid Certificates</a>
 
 
 </head>
 <body style="background-color:Black;">
 <h1 style="color:Lime;"> 1st application</h1>
-<p1 style="color:Lime;"> I have two favorite applications, the first would have to be game master 5 because it’s the main
+<p1 style="color:Lime;"> I have five favorite applications, the first would have to be game master 5 because it’s the main
 tool I use when I play with my friends. I like using this app because it has all the tools I need to create
 and control our DnD games. It has all the items and rules already installed in it and it allows us to keep
 track of our own characters as well.</p1>
+<img src="GM5.jpg" alt="You Are Hereby" style="width:300px;height:400px;">
 
-<h2 style="color:Lime;"> 2nd application</h2>
-<p2 style="color:Lime;">My 2nd favorite application is spotify because it has a large collection of music from many different
+<h2 style="color:Chartreuse ;"> 2nd application</h2>
+<p2 style="color:Chartreuse ;">My 2nd favorite application is spotify because it has a large collection of music from many different
 genres and artists. This is one of my favorite apps because during my free time or when I’m on the road I
 love to listen to music to pass the time. One of the main downsides though about spotify is that you
 must listen through advertisements not unless you buy the premium version but overall it’s a very good
 music application.</p2>
-
-<h3 style="color:Lime;"> 3rd application</h3>
-<p3 style="color:Lime;">Dropbox would have to be my third favorite application because it’s a quick and easy file sharing
+<img src="SPOT.jpg" alt="You Are Hereby" style="width:300px;height:400px;">
+<h3 style="color:MediumAquaMarine ;"> 3rd application</h3>
+<p3 style="color:MediumAquaMarine ;">Dropbox would have to be my third favorite application because it’s a quick and easy file sharing
 application. It allows me and others to upload important files online that way even if we lose the original
 copy its saved online for future use. It also allows us to share those files with other people and they can
 add their own files to it so they can share it with you</p3>
-
-<h4 style="color:Lime;"> 4th application</h4>
-<p4 style="color:Lime;">Facebook is my fourth favorite app because it’s an easy way for me to pass the time when I have
+<img src="DROP.jpg" alt="You Are Hereby" style="width:300px;height:400px;">
+<h4 style="color:SeaGreen ;"> 4th application</h4>
+<p4 style="color:SeaGreen ;">Facebook is my fourth favorite app because it’s an easy way for me to pass the time when I have
 nothing to do plus, I can see what my friends have shared and react to what they posted. It’s also a way
 for us to comment on the posts and it can them to comment back to us.</p4>
-
-<h5 style="color:Lime;"> 5th application</h5>
-<p5 style="color:Lime;"> Fate Grand Order is my last favorite app because it’s a good way for me to pass the time and even
+<img src="FACE.jpg" alt="You Are Hereby" style="width:300px;height:400px;">
+<h5 style="color:NavajoWhite ;"> 5th application</h5>
+<p5 style="color:NavajoWhite ;"> Fate Grand Order is my last favorite app because it’s a good way for me to pass the time and even
 though it seems simple the game is quite difficult when the challenge quests come around because you
 have to plan your team and how to use them to their fullest. I also love the art style they use in the
 game because they have all their own distinct way of looking. The story is also something that has
 interested me because it isn’t something normally seen.</p5>
-
+<img src="FATE.jpg" alt="You Are Hereby" style="width:300px;height:400px;">
 </body>
 </html>
