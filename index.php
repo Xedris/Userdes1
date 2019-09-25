cd1d1fc77bc259420e916398742ed110e22ecfc4
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 22:49:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<a href="index.php"style="color:Black;"><font size="4">HOMEPAGE</font></a>
-<a href="new-2.php"style="color:Black;"><font size="4">My Likes and dislikes</font></a>
-<a href="Activity2.php"style="color:Black;"><font size="4">My Favorite apps</font></a>
-<a href="LABWORK1_Fortuno.php"style="color:Black;"><font size="4">My Labwork 1</font></a>
-<a href="Activity3.php"php"style="color:Black;"><font size="4">1000 Floor Elevator</font></a>
-<a href="Activity-6.php"style="color:Black;"><font size="4">Invitation</font></a>                                                              
-<a href="Activity-7.php"style="color:Black;"><font size="4">Invitation</font></a>
-<a href="Grid.php"style="color:Black;"><font size="4">Grid Designs</font></a>
-<a href="Reflections.php"style="color:Black;"><font size="4">Reflections</font></a>
-<a href="UX-DESIGN-CERTIFICATES.php"style="color:Black;"><font size="4">Vid Certificates</font></a>
+<a href="index.php"style="color:White;"><font size="4">HOMEPAGE</font></a>
+<a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
+<a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
+<a href="LABWORK1_Fortuno.php"style="color:White;"><font size="4">My Labwork 1</font></a>
+<a href="Activity3.php"php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity-6.php"style="color:White;"><font size="4">Invitation</font></a>                                                              
+<a href="Activity-7.php"style="color:White;"><font size="4">Invitation</font></a>
+<a href="Grid.php"style="color:White;"><font size="4">Grid Designs</font></a>
+<a href="Reflections.php"style="color:White;"><font size="4">Reflections</font></a>
+<a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
 </head>
 <style>
 body {
@@ -21,7 +21,9 @@ body {
 }
 </style>
 <h1>FORTUNO, ADRIAN CHRISTIAN O</h1>
-<P>Hi there, names Adrian and this is a short Bio about me and what I am. I just recently turned 20 last month on the 18th of june and it has been a weird 20 years of life. I've never much fit in with regular people, I've always weirded them out with the stuff I like since it's not something that's normal for them.</P>
+<P><font size="4.5">Hi there, names Adrian and this is a short Bio about me and what I am. I just recently turned 20 last month on the 18th 
+                 of june and it has been a weird 20 years of life. I've never much fit in with regular people, I've always weirded them 
+                 out with the stuff I like since it's not something that's normal for them.</font></P>
 
 
 
