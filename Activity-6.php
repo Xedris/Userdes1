b546e76ae9f659aae4625d8a5a8cd04de3cba587
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 23:06:10 +0800

    Update Activity-6.php

--- Activity-6.php
+++ Activity-6.php
@@ -22,18 +22,18 @@ body {
 </style>
   <h1> PERSONA!! </h1>
 <h1 style="color:Lime;"> Persona 1</h1>
-<p1 style="color:Lime;"> Dwayne is a student here in Asia Pacific College. He spends lots of his time working on his projects in 
+<p1 style="color:Lime;"><font size="5"> Dwayne is a student here in Asia Pacific College. He spends lots of his time working on his projects in 
   his engineering class, and for the past few days, he’s been feeling rather loaded with work and exams. Now that exams are over, 
   he wants to spend some quality time with his friends after classes. 
 He then remembers his friend telling him about the Gaming Laboratory on the 5th floor, and that they should pay a visit one day. 
   He checks his schedule and sees that he’s free to visit with his group later in the afternoon, but is unsure if the lab is available.
 Dwayne asks one of his professors, Sir Isaac, about the lab’s hours, to which he is told of the new web application that allows users to
   set schedules and activities. He finds a slot that fits his schedule, and arrives with his friends at the appointed time to see some 
-  others in the room. They find their reserved place and begin gaming.
+  others in the room. They find their reserved place and begin gaming.</font>
 </p1>
   <h5> <img src="Persona 1.jpg" alt="Dwayne" width="500" height="600"></h5>
 <h1 style="color:Lime;"> Persona 2</h1>
-  <p2 style="color:Lime;"> Sir Isaac is a Game Development Professor here at Asia Pacific College who also happens to be one of the founding members of 
+  <p2 style="color:Lime;"> <font size="5">Sir Isaac is a Game Development Professor here at Asia Pacific College who also happens to be one of the founding members of 
     Gaming Genesis. Aside from spending time gaming with his students, and moderating the activities in the room, he also uses the 
     area for some of his classes when needed.
 On one particular day, he decides it would be best to deliver his lecture in the lab, but wants to do so without interfering with the 
@@ -41,11 +41,11 @@ On one particular day, he decides it would be best to deliver his lecture in the
     schedule and sets it. 
 One of his irregular students, Dwayne, sees the lecture-based reservation while browsing the website for a schedule, and recognizes that 
     the room will be reserved entirely for that period. He accepts the case and is thankful that he was informed well ahead of time to 
-    choose another schedule.
+    choose another schedule.</font>
   </p2>
   <h6><img src="Persona 2.jpg" alt="Isaac" width="500" height="600"></h6>
 <h1 style="color:Lime;"> Persona 3</h1>
- <p3 style="color:Lime;"> Miss Mara is an employee here at Asia Pacific College as part of the ITRO. Aside from managing the websites and other systems 
+ <p3 style="color:Lime;"><font size="5"> Miss Mara is an employee here at Asia Pacific College as part of the ITRO. Aside from managing the websites and other systems 
    that keep the school running, she is also partly responsible for watching over the borrowed equipment and peripherals of the school.
 Mara happens to also be one of the early developers of the Game laboratory Reservation System, and regularly uses it to monitor 
    equipment use.
@@ -54,7 +54,7 @@ On one of her checks over the room, she discovers that one of the Xbox controlle
 She informs Isaac, who is one of the head moderators of the room. Isaac, who is a professor, recognizes Dwayne and approaches him
    regarding the matter. He then finds out that Dwayne had mistakenly dropped the controller when heading out, but didn’t think the 
    damage too severe. Seeing the case, Dwayne makes the appropriate payments for repairs.
-
+                                       </font>
   </p3>
   <h7><img src="Persona 3.jpg" alt="Mara" width="500" height="600"></h7>
 </body>
