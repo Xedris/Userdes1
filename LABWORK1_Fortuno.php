719c00becf0653e542751fa1ed4dc7ba4d1b593b
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 10:03:23 +0800

    Update LABWORK1_Fortuno.php

--- LABWORK1_Fortuno.php
+++ LABWORK1_Fortuno.php
@@ -4,12 +4,13 @@
 <title><b> JUST CALL ME AIDS </b> </title>
 <a href="index.php">HOMEPAGE</a>
 <a href="new-2.php">My Likes and dislikes</a>
-<a href="Activity2.php">Activity2</a>
-<a href="Reflections.php">Reflections</a>
+<a href="Activity2.php">My Favorite Apps</a>
+
 <a href="Activity3.php">1000 Floor Elevator</a>
 <a href="Activity-6.php">Persona</a>
 <a href="Activity-7.php">Invitation</a>
 <a href="Grid.php">Grid Designs</a>
+<a href="Reflections.php">Reflections</a>
 <a href="UX-DESIGN-CERTIFICATES.php">Vid Certificates</a>
 </head>
 <body style="background-color:Black;">
@@ -18,14 +19,14 @@
 Yes that is my nickname I've had it with me for a couple years now ever since grade 8. At first it really annoyed me
 but then I started to get used to it until it became my nickname till now. It's actually funny since over the years I've gained more STD related nicknames like HIV, Ebola, and even STD. Most people wouldn't like being called that but I got so used to being called that it feels weird being called something else now. </p1>
 
-<h2 style="color:Saddlebrown;"> My Personality</h2>
+<h1 style="color:Saddlebrown;"> My Personality</h1>
 <p2 style="color:Saddlebrown;">I am an ambivert but i lean more to the introverted side but i become more extroverted when i spend time with close friends. My music tastes are different than others because i dont listen to mainstream music, I like to listen alternate rock and pop especially Nightcore versions of songs since i like it when the songs are more fast paced. I have a hard time expressing my feelings most of the time and I put on a mask of either excitedness or indifference to most people. I used to be more emotional but after some experiences with bullying in my old school i started to bottle up my emotions and created a no care attitude towards others. But since im not out of that place I've started to open up some more to people </p2>
 
-<h3 style="color:Moccasin;"> Summer Vacation</h3>
+<h1 style="color:Moccasin;"> Summer Vacation</h1>
 <p3 style="color:Moccasin;">        My summer was mostly me at home playing Borderlands 2 or Mortal Kombat 11 on my PS4 but in the 2nd to the last week of vacation my parents and I went to Cebu for a family trip. It was fun since we were able to swim with the whale sharks and swam under a waterfall, the waterfall was exciting, but it was so cold. It was also the first time i went out of the Luzon region</p3>
-<h3 style="color:Royalblue;">My Expectations</h3>
+<h1 style="color:Royalblue;">My Expectations</h1>
 <p3 style="color:Royalblue;">       My expectations to for this class would be to at least learn how most UI interfaces are designed and i guess how to manipulate it.</p3>
-<h4 style="color:Lightsalmon">What is UI</h4>
+<h1 style="color:Lightsalmon">What is UI</h1>
 <p4 style="color:Lightsalmon">        User Interfaces from what I can understand is the tools a person uses when there using any form of Application or tool. It is what allows the program to be used by others and it can help navigate through all the other tools given by the creator.</p4>
 
 
