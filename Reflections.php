8a5bb9eef021c74f72122e314d323f8454e192d9
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-25 10:23:22 +0800

    Update Reflections.php

--- Reflections.php
+++ Reflections.php
@@ -20,27 +20,27 @@ to express ourselves. I liked doing the one that asked us to talk about or favor
 one gives and uses. For the teacher I like the way Ms is doing the class but only doing activities is a bit weird but I think I can 
 adjust well.</p1>
 
-<h2 style="color:DeepPink;"> Reflection 2 </h2>
+<h1 style="color:DeepPink;"> Reflection 2 </h1>
 <p2 style="color:DeepPink;">My reflection about the 2nd week of the class is that things are moving too fast and that there is 
 almost no time to understand everything. There is also to many things to use at the same time especially Heroku since it’s hard to 
 use and understand. But as for the activities itself they are fun to do especially with how we must solve the problems. As for the 
 class itself it’s still nice to code but all the continous work is starting to bog us down.</p2>
 
-<h3 style="color:LightGreen;"> Reflection 3</h3>
+<h1 style="color:LightGreen;"> Reflection 3</h1>
 <p3 style="color:LightGreen;">So the moment ms gave us the Lab 1 activity I was excited to do it because it was something I haven’t done 
 in a long time and it was something I used to do before when I was in 3rd year highschool. When we all learned that we had to upload 
 the HTML into both heroku and github it was actually confusing on how to make heroku work especially since for so long we’ve been using
 only Github for our work and heroku was brand new for us to work with. I actually was starting to think that heroku was busted or 
 something since it had no user manual to use but once we got it to work it was actually easy to understand more or less</p3>
 
-<h4 style="color:MediumOrchid;"> Reflection 4</h4>
+<h1 style="color:MediumOrchid;"> Reflection 4</h1>
 <p4 style="color:MediumOrchid;">After the lesson when talking about personas and UI I really did not care much about what went on in the UI
 since I only cared about if it was useful or not. Even now I only care if the UI is simple enough to use and navigate for me, I prefer
   a UI that is this way because simplicity is always the best when creating a navigation for public use. This is because it is easy to get
   confused when the UI is packed with large amounts of info and items and it gets bogged down with to many add-ons.</p4>
 <img src="QUIZ 1.JPG" alt="You Are Hereby" style="width:500px;height:600px;">
   
-<h5 style="color:MediumOrchid;"> Reflection 5</h5>
+<h1 style="color:MediumOrchid;"> Reflection 5</h1>
 <p5 style="color:MediumOrchid;">The entire SOCIT fest in its entirety can be summed up for me in one word and that is disappointing. 
   It’s disappointing because most of the activities we did was just seminar after seminar and even though some were ok the rest was 
   just not interesting to me to grasp what was even said. As for the only other event I was, it was the Tekken Tournament hosted by 
@@ -58,7 +58,7 @@ since I only cared about if it was useful or not. Even now I only care if the UI
 <img src="68824331_893030544385985_8899031405334364160_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;">
 <img src="68853843_488545965304425_6326106636502958080_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;">
 
-<h6 style="color:MediumOrchid;"> Reflection 6</h6>
+<h1 style="color:MediumOrchid;"> Reflection 6</h1>
 <p6 style="color:MediumOrchid;">The first half of USERDES was not what I was expecting since I expected more web designing or UI design 
   by using websites or photoshop, and not by doing written activities mostly. The main Challenges I encountered were the LABWORKS since 
   my laptop is not optimized and I had to ask help from my classmates. Number 2 made me sad since it just makes me remember that I want 
