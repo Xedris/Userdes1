89af12f6b6adbbb92341e0ff3256af1f2e205c34
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 16:52:39 +0800

    Update Grid.php

--- Grid.php
+++ Grid.php
@@ -14,14 +14,15 @@
 <h1>3x3 Foundations</h1>
 <p><img src="3x3 white and black.JPG" alt="3x3 foundations"></p>
 <h1>The King of YOUTUBE</h1>
-<p1> The reason why i chose pewdiepie for the cover for my 3x3 foundations was because he's one of my favorite youtubers and the most popularone.
+<p1><font size="5"> The reason why i chose pewdiepie for the cover for my 3x3 foundations was because he's one of my favorite youtubers and the most popularone.
   The main reason why though is because of the quotes he gave, because even though some are funny or just random sayings, some of the words
-  he says is something that i listen to even to this day</p1>
+  he says is something that i listen to even to this day</font></p1>
 <p><img src="3x3 pictures.JPG" alt="The King Of YOUTUBE"></p>
 <h1>4x4 Foundation</h1>
 <p><img src="4x4 black and white.JPG" alt="4x4 Foundations"></p>
 <h1>FATE GRAND ORDER</h1>
-<p2> I chose Fate Grand Order for the pictures because it is one of my favorite franchises and because the game is fun to play.</p2>
+<p2><font size="5"> I chose Fate Grand Order for the pictures because it is one of my favorite franchises and because the game is fun to play.
+  </font></p2>
 <p><img src="4x4 pictures.JPG" alt="FATE GRAND ORDER"></p>
 </body>
 </html>
