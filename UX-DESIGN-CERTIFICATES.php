aa04d11c6a0c01c3e3e7e2f0c4f81ec773b683e6
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 15:46:55 +0800

    Update UX-DESIGN-CERTIFICATES.php

--- UX-DESIGN-CERTIFICATES.php
+++ UX-DESIGN-CERTIFICATES.php
@@ -23,6 +23,15 @@
 <h1>Learning SQL Programming</h1>
 <p4><img src="cert 5.JPG" alt="INTERACTION DESIGN: PROJECTS AND PLATFORMS"></p4>
 <h1>Database Foundations: Administration</h1>
-<p6><img src="cert 6.JPG" alt="UX FOUNDATIONS: INTERACTION DESIGN"></p6>
+<p6><img src="cert 6.JPG" alt="UX FOUNDATIONS: INTERACTION DESIGN"></p6><br>
+<a href="index.php"style="color:Black;"><font size="4">HOMEPAGE</font></a>
+<a href="new-2.php"style="color:Black;"><font size="4">My Likes and dislikes</font></a>
+<a href="Activity2.php"style="color:Black;"><font size="4">My Favorite apps</font></a>
+<a href="LABWORK1_Fortuno.php"style="color:Black;"><font size="4">My Labwork 1</font></a>
+<a href="Activity3.php"php"style="color:Black;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity-6.php"style="color:Black;"><font size="4">Persona</font></a>                                                              
+<a href="Activity-7.php"style="color:Black;"><font size="4">Invitation</font></a>
+<a href="Grid.php"style="color:Black;"><font size="4">Grid Designs</font></a>
+<a href="Reflections.php"style="color:Black;"><font size="4">Reflections</font></a>                                                
 </body>
 </html>
