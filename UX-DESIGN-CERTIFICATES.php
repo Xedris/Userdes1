e14fb27a7f8cd26062e440100d0b49c300a03aa1
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 16:02:06 +0800

    Update UX-DESIGN-CERTIFICATES.php

--- UX-DESIGN-CERTIFICATES.php
+++ UX-DESIGN-CERTIFICATES.php
@@ -12,6 +12,8 @@
 <a href="Grid.php"style="color:Black;"><font size="4">Grid Designs</font></a>
 <a href="Reflections.php"style="color:Black;"><font size="4">Reflections</font></a>
 </head>
+ <div class="w3-third">
+  <div class="w3-card">                                                          
 <h1>UX FOUNDATIONS: CONTENT STRATEGY</h1>
 <p2><img src="cert 1.JPG" class="w3-border w3-padding" alt="UX FOUNDATIONS: CONTENT STRATEGY"></p2>
 <h1>INTERACTION DESIGN: PROJECTS AND PLATFORMS</h1>
@@ -24,6 +26,11 @@
 <p4><img src="cert 5.JPG" class="w3-border w3-padding" alt="INTERACTION DESIGN: PROJECTS AND PLATFORMS"></p4>
 <h1>Database Foundations: Administration</h1>
 <p6><img src="cert 6.JPG" class="w3-border w3-padding" alt="UX FOUNDATIONS: INTERACTION DESIGN"></p6><br>
+  <div class="w3-container">
+    
+    </div>
+  </div>
+</div>                                                                              
 <a href="index.php"style="color:Black;"><font size="4">HOMEPAGE</font></a>
 <a href="new-2.php"style="color:Black;"><font size="4">My Likes and dislikes</font></a>
 <a href="Activity2.php"style="color:Black;"><font size="4">My Favorite apps</font></a>
