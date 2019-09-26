cf31adbbb30a0caaffbcc6de7e4d108817699330
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 22:33:46 +0800

    Update Activity4.php

--- Activity4.php
+++ Activity4.php
@@ -6,10 +6,10 @@
 <a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
 <a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
 <a href="LABWORK1_Fortuno.php"style="color:White;"><font size="4">My Labwork 1</font></a>
-<a href="Activity3.php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>                                                           
+<a href="Activity3.php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity4.php"style="color:White;"><font size="4">Need finding</font></a>                                                            
 <a href="Activity-6.php"style="color:White;"><font size="4">Persona</font></a>                                                              
 <a href="Activity-7.php"style="color:White;"><font size="4">Invitation</font></a>
-<a href="Grid.php"style="color:White;"><font size="4">Grid Designs</font></a>
 <a href="Reflections.php"style="color:White;"><font size="4">Reflections</font></a>
 <a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
 </head>
@@ -32,10 +32,10 @@ to the students and professors who will be using the equipment in the room.</fon
 <a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
 <a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
 <a href="LABWORK1_Fortuno.php"style="color:White;"><font size="4">My Labwork 1</font></a>
-<a href="Activity3.php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>                                                            
+<a href="Activity3.php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity4.php"style="color:White;"><font size="4">Need finding</font></a>                                                            
 <a href="Activity-6.php"style="color:White;"><font size="4">Persona</font></a>                                                              
 <a href="Activity-7.php"style="color:White;"><font size="4">Invitation</font></a>
-<a href="Grid.php"style="color:White;"><font size="4">Grid Designs</font></a>
 <a href="Reflections.php"style="color:White;"><font size="4">Reflections</font></a>
 <a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
 </body>
