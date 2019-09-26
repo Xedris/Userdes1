7a6121b69c1a1119b8cfcd3e4eedd038afab0f7f
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 20:53:00 +0800

    Update Activity4.php

--- Activity4.php
+++ Activity4.php
@@ -13,8 +13,8 @@
 <a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
 </head>
 <body style="background-color:Black;">
-<h1 style="color:Lime;"> Need Finding</h1>
-<p1 style="color:Lime;"><font size="5"> Need finding from i can tell is just finding the specific needs for your innovation. It helps keep
+<h1 style="color:DarCyan;"> Need Finding</h1>
+<p1 style="color:DarkGoldenRod;"><font size="5"> Need finding from i can tell is just finding the specific needs for your innovation. It helps keep
 track of your goal and what specifics are needed to answer its problems.<br>
 The topic we chose for our project is a Reservation system for the new gaming laboratory of APC. At the moment its still in its infancy stage
 and new innovations are being put it all the time to help make the room have an easier experience. We believe that a reservation system
@@ -25,6 +25,8 @@ The main goal of the system would be to<br>
 <br>
 We want our project to be able in assisting the officers in managing the room at all times as well as installing a sense of accountability
 to the students and professors who will be using the equipment in the room.</font></p1><br>
+                                      <br>
+                                      <br>
 <a href="index.php"style="color:White;"><font size="4">HOMEPAGE</font></a>
 <a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
 <a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
