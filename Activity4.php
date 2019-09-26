d927b0dd56eea0b6dfdc1462bd6728edacaaa7b6
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 20:48:08 +0800

    Create Activity4.php

---
+++ Activity4.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><b> Reflections </b> </title>
+<a href="index.php"style="color:White;"><font size="4">HOMEPAGE</font></a>
+<a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
+<a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
+<a href="LABWORK1_Fortuno.php"style="color:White;"><font size="4">My Labwork 1</font></a>
+<a href="Activity3.php"php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity-6.php"style="color:White;"><font size="4">Persona</font></a>                                                              
+<a href="Activity-7.php"style="color:White;"><font size="4">Invitation</font></a>
+<a href="Grid.php"style="color:White;"><font size="4">Grid Designs</font></a>
+<a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
+</head>
+<body style="background-color:Black;">
+<h1 style="color:Lime;"> Need Finding</h1>
+<p1 style="color:Lime;"><font size="5"> Need finding from i can tell is just finding the specific needs for your innovation. It helps keep
+track of your goal and what specifics are needed to answer its problems.<br>
+The topic we chose for our project is a Reservation system for the new gaming laboratory of APC. At the moment its still in its infancy stage
+and new innovations are being put it all the time to help make the room have an easier experience. We believe that a reservation system
+will help the officers keep track of the goings on in the room and who is using what item.<br>
+The main goal of the system would be to<br>
+1. Maintain control of the population of the room so it does not over crowd<br>
+2. Make sure that each item is accounted for after the end of the day.<br>
+<br>
+We want our project to be able in assisting the officers in managing the room at all times as well as installing a sense of accountability
+to the students and professors who will be using the equipment in the room.</font></p1><br>
+<a href="index.php"style="color:White;"><font size="4">HOMEPAGE</font></a>
+<a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
+<a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
+<a href="LABWORK1_Fortuno.php"style="color:White;"><font size="4">My Labwork 1</font></a>
+<a href="Activity3.php"php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity-6.php"style="color:White;"><font size="4">Persona</font></a>                                                              
+<a href="Activity-7.php"style="color:White;"><font size="4">Invitation</font></a>
+<a href="Grid.php"style="color:White;"><font size="4">Grid Designs</font></a>
+<a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
+</body>
+</html>
