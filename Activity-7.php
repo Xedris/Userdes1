d3ff4370ee3c7c58233109ec0af9a6147785cd9f
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 09:49:41 +0800

    Update Activity-7.php

--- Activity-7.php
+++ Activity-7.php
@@ -13,6 +13,17 @@
 
 </head>
 <h1>You are Hereby Invited</h1>
-<img src="Activity.7.jpg" alt="You Are Hereby">
+<img src="Activity.7.jpg" alt="You Are Hereby"><br>
+  <p> <b>How can we improve this?</b> <br>
+    a. One of the things that you can do to improve the invitation is to get rid of word<br>
+    "delectable" from the wording since its used to describe food more instead of an event<br>
+    b. You can also specify the location more since "The Pad" is very vague and could be a location<br>
+    anywhere or it could be an inside joke between them<br>
+    c. The when and where is located in the wrong place and there should already be a space for both<br>
+    <br>
+    2. What is the Objective Function?<br>
+    The Objective Function is to be an invitation to close friends or colleagues to an after dinner part<br>
+    hosted by Robert and Alexandria</p>
+    
 </body>
 </html>
