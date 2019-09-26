76e999962247c8c049b493a92ac5ef645aef5791
Xedris <52141557+Xedris@users.noreply.github.com>
2019-09-26 18:54:00 +0800

    Update Reflections.php

--- Reflections.php
+++ Reflections.php
@@ -2,47 +2,46 @@
 <html>
 <head>
 <title><b> Reflections </b> </title>
-<a href="index.php">HOMEPAGE</a>
-<a href="LABWORK1_Fortuno.php">My Labwork 1</a>
-<a href="new-2.php">My Likes and dislikes</a>
-<a href="Activity2.php">My Favorite apps</a>
-<a href="Activity3.php">1000 Floor Elevator</a>
-<a href="Activity4.php">Need Finding</a>
-<a href="Activity-6.php">Persona</a>
-<a href="Activity-7.php">Invitation</a>
-<a href="Grid.php">Grid Designs</a>
-<a href="UX-DESIGN-CERTIFICATES.php">Vid Certificates</a>
+<a href="index.php"style="color:White;"><font size="4">HOMEPAGE</font></a>
+<a href="new-2.php"style="color:White;"><font size="4">My Likes and dislikes</font></a>
+<a href="Activity2.php"style="color:White;"><font size="4">My Favorite apps</font></a>
+<a href="LABWORK1_Fortuno.php"style="color:White;"><font size="4">My Labwork 1</font></a>
+<a href="Activity3.php"php"style="color:White;"><font size="4">1000 Floor Elevator</font></a>
+<a href="Activity-6.php"style="color:White;"><font size="4">Persona</font></a>                                                              
+<a href="Activity-7.php"style="color:White;"><font size="4">Invitation</font></a>
+<a href="Grid.php"style="color:White;"><font size="4">Grid Designs</font></a>
+<a href="UX-DESIGN-CERTIFICATES.php"style="color:White;"><font size="4">Vid Certificates</font></a>
 </head>
 <body style="background-color:Black;">
 <h1 style="color:Lime;"> Reflection 1</h1>
-<p1 style="color:Lime;"> The first week of the class was actually quite surprising since I didn’t expect to be using trello, 
+<p1 style="color:Lime;"><font size="5"> The first week of the class was actually quite surprising since I didn’t expect to be using trello, 
 I didn’t even know trello was a thing at first until Ms showed it to us. As for the activities I liked doing them since it allows us 
 to express ourselves. I liked doing the one that asked us to talk about or favorite app since I like to talk about the features each
 one gives and uses. For the teacher I like the way Ms is doing the class but only doing activities is a bit weird but I think I can 
-adjust well.</p1>
+adjust well.</font></p1>
 
 <h1 style="color:DeepPink;"> Reflection 2 </h1>
-<p2 style="color:DeepPink;">My reflection about the 2nd week of the class is that things are moving too fast and that there is 
+<p2 style="color:DeepPink;"><font size="5">My reflection about the 2nd week of the class is that things are moving too fast and that there is 
 almost no time to understand everything. There is also to many things to use at the same time especially Heroku since it’s hard to 
 use and understand. But as for the activities itself they are fun to do especially with how we must solve the problems. As for the 
-class itself it’s still nice to code but all the continous work is starting to bog us down.</p2>
+class itself it’s still nice to code but all the continous work is starting to bog us down.</font></p2>
 
 <h1 style="color:LightGreen;"> Reflection 3</h1>
-<p3 style="color:LightGreen;">So the moment ms gave us the Lab 1 activity I was excited to do it because it was something I haven’t done 
+<p3 style="color:LightGreen;"><font size="5">So the moment ms gave us the Lab 1 activity I was excited to do it because it was something I haven’t done 
 in a long time and it was something I used to do before when I was in 3rd year highschool. When we all learned that we had to upload 
 the HTML into both heroku and github it was actually confusing on how to make heroku work especially since for so long we’ve been using
 only Github for our work and heroku was brand new for us to work with. I actually was starting to think that heroku was busted or 
-something since it had no user manual to use but once we got it to work it was actually easy to understand more or less</p3>
+something since it had no user manual to use but once we got it to work it was actually easy to understand more or less</font></p3>
 
 <h1 style="color:MediumOrchid;"> Reflection 4</h1>
-<p4 style="color:MediumOrchid;">After the lesson when talking about personas and UI I really did not care much about what went on in the UI
+<p4 style="color:MediumOrchid;"><font size="5">After the lesson when talking about personas and UI I really did not care much about what went on in the UI
 since I only cared about if it was useful or not. Even now I only care if the UI is simple enough to use and navigate for me, I prefer
   a UI that is this way because simplicity is always the best when creating a navigation for public use. This is because it is easy to get
-  confused when the UI is packed with large amounts of info and items and it gets bogged down with to many add-ons.</p4>
+  confused when the UI is packed with large amounts of info and items and it gets bogged down with to many add-ons.</font></p4><br>
 <img src="QUIZ 1.JPG" alt="You Are Hereby" style="width:500px;height:600px;">
   
 <h1 style="color:MediumOrchid;"> Reflection 5</h1>
-<p5 style="color:MediumOrchid;">The entire SOCIT fest in its entirety can be summed up for me in one word and that is disappointing. 
+<p5 style="color:MediumOrchid;"><font size="5">The entire SOCIT fest in its entirety can be summed up for me in one word and that is disappointing. 
   It’s disappointing because most of the activities we did was just seminar after seminar and even though some were ok the rest was 
   just not interesting to me to grasp what was even said. As for the only other event I was, it was the Tekken Tournament hosted by 
   Gaming Genesis, now that was an event I was excited for since I love to watch Fighting Game Competitions, and because I can see 
@@ -53,21 +52,21 @@ since I only cared about if it was useful or not. Even now I only care if the UI
   The event that I liked the most was the Tekken tournament by Gaming Genesis, since I love video games and watching fighting game 
   tournaments is one of my favorite things to do. The other events need better improvement especially the seminar’s, having seminar
   after seminar is not a fun activity to do. For next year’s SOCIT week, I just want other activities other than seminars because 
-  they become boring and repetitive.
-.</p5>
-<img src="68741961_1159532210912244_5937637002177413120_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;">
-<img src="68824331_893030544385985_8899031405334364160_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;">
-<img src="68853843_488545965304425_6326106636502958080_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;">
+  they become boring and repetitive.<br>
+.</font></p5>
+<img src="68741961_1159532210912244_5937637002177413120_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;"><br>
+<img src="68824331_893030544385985_8899031405334364160_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;"><br>
+<img src="68853843_488545965304425_6326106636502958080_n.jpg" alt="You Are Hereby" style="width:500px;height:600px;"><br>
 
 <h1 style="color:MediumOrchid;"> Reflection 6</h1>
-<p6 style="color:MediumOrchid;">The first half of USERDES was not what I was expecting since I expected more web designing or UI design 
+<p6 style="color:MediumOrchid;"><font size="5">The first half of USERDES was not what I was expecting since I expected more web designing or UI design 
   by using websites or photoshop, and not by doing written activities mostly. The main Challenges I encountered were the LABWORKS since 
   my laptop is not optimized and I had to ask help from my classmates. Number 2 made me sad since it just makes me remember that I want 
   a new laptop, but I have to save enough money for it and that will take a while. My main expectation for the finals is just to lessen
   the amount of activities that we do back to back. The only thing I can say that needs improvement is that activities should not be
   put back to back. Regarding the grades a lot of us would not be satisfied if Rosento gets a high grade, the reason is that he’s 
   either always late or absent to class and that he does not help us in the Project and he was absent during the presentation and 
-  if he gets a high grade even when he was absent there I will be really mad about that.</p6>
+                                              if he gets a high grade even when he was absent there I will be really mad about that.</font></p6>
 
   
 
